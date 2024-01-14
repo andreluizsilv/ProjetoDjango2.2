29e3d40d6985e591c733714cf0a4d48081741546
Bowie <andre_luiz29@hotmail.com>
2024-01-14 09:48:04 -0300

    deploy railway

---
+++ README.md
@@ -0,0 +1,2 @@
+# ProjetoDjango2
+ NovoProjeto
---
+++ filme/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Filme, Episodio, Usuario
+from django.contrib.auth.admin import UserAdmin
+
+# Criando Campo no Admin filmes_visto
+campos = list(UserAdmin.fieldsets)
+campos.append(
+    ("Historico", {'fields': ('filmes_vistos',)})
+)
+UserAdmin.fieldsets = tuple(campos)
+
+
+# Register your models here.
+admin.site.register(Filme)
+admin.site.register(Episodio)
+admin.site.register(Usuario, UserAdmin)
+
